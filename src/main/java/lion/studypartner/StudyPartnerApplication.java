43ad1f11bce0055c1b81f7aa1ea4910c37016edf
Hyun-jun-Lee0811 <korong0811@naver.com>
2025-07-07 23:32:39 +0900

    Initial commit: Study Partner App

---
+++ src/main/java/lion/studypartner/StudyPartnerApplication.java
@@ -0,0 +1,13 @@
+package lion.studypartner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPartnerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyPartnerApplication.class, args);
+  }
+
+}
